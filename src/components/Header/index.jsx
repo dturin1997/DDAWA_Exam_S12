49ad4affc338479e98e7d4fdf1315b7ca0050349
MAC44 <mac44@MacBook-Pro-de-MAC44.local>
2022-11-11 11:15:41 -0500

    Se agrego diseño responsivo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,11 +5,11 @@ const Header = ({handleChange}) => {
   return (
     <header className="header w-100">
       <div className="header_elements">
-        <div className="header_elements_img">
-            <img src="./deadful_tomatoes.png" alt="logo"/>
+        <div className="header_elements_img col-md-12">
+            <img src="./deadful_tomatoes.png" alt="logo" className="img-fluid"/>
         </div>
         <div className="header_elements_search">
-            <div className="header_elements_search_group">
+            <div className="header_elements_search_group col-md-12">
                 <i class="bi bi-search"></i>
                 <input placeholder="Title" onChange={handleChange}/>
             </div>    
